ae13d981eeb03a1cefd8a8853e32a76dae90b86c
Steph Peets <stephanie.peets@gmail.com>
2022-01-24 17:11:40 -0500

    add button update funtionality

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
       <div className="app">
         <div className="card">
           <h1 className="heading">{advice}</h1>
-          <button className="button">
+          <button className="button" onClick={ this.fetchLinks }>
               <span>LINK PLEASE!</span>
           </button>
         </div>
