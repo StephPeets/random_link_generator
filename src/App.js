eac93a455678da0d33ed66644919d26227e25f70
Steph Peets <stephanie.peets@gmail.com>
2022-01-24 15:54:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = { link: "" };
+
+  componentDidMount() {
+    this.fetchLinks();
+  }
+
+  fetchLinks = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice }); // is actually{ advice: advice }, but since key/object are same, can be abbreviated
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
